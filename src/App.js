5819e4cee543e27ef9e5a3a1d3db561973dd4f33
eparubrub <emmit.parubrub@gmail.com>
2022-03-03 20:33:30 -0800

    translating from function to class App

--- src/App.js
+++ src/App.js
@@ -1,76 +1,85 @@
-import { useState, useEffect } from "react";
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 import { db } from './firebase-config';
 import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc} from 'firebase/firestore';
 
-function App() {
-  const [newName, setNewName] = useState("");
-  const [newAge, setNewAge] = useState(0);
 
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
-
-  const createUser = async () => {
-    await addDoc(usersCollectionRef, {name: newName, age: Number(newAge)});
+class App extends React.Component {
+  state = {
+    newName: '',
+    newAge: 0,
+    users: []
   };
 
-  const upddateUser = async (id, age) => {
-    const userDoc = doc(db, "users", id)
-    const newFields = {age: age + 1}
-    await updateDoc(userDoc, newFields)
+  setName = async (name) => {
+    this.setState({newName: name});
   }
 
-  const deleteUser = async (id) => {
-    const userDoc = doc(db, "users", id)
-    await deleteDoc(userDoc);
+  setAge = async (age) => {
+    this.setState({newAge: age});
+  }
+
+  createUser = async () => {
+    await addDoc(collection(db, "users"), {
+      name: this.state.newName, 
+      age: Number(this.state.newAge)
+    });
+  };
+
+  upddateUser = async (id, age) => {
+    const newFields = {age: age + 1}
+    await updateDoc(doc(db, "users", id), newFields)
+  }
+
+  deleteUser = async (id) => {
+    await deleteDoc(doc(db, "users", id));
   };
 
 
-  useEffect(() =>{
+  componentDidMount = async () => {
+    const data = await getDocs(collection(db, "users"));
+    this.setState({
+      users: data.docs.map((doc) => ({...doc.data(), id: doc.id }))  
+    });
+  }
 
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
-    };
 
-    getUsers();
-  }, []);
-
-  return (
-    <div className="App">
-      <input 
-        placeholder="Name..." 
-        onChange={(event) => {
-          setNewName(event.target.value)
-        }}
-      />
-      <input 
-        type ="number" 
-        placeholder="Age..."
-        onChange={(event) => {
-          setNewAge(event.target.value)
-        }}
-      />
-      <button onClick={createUser}>Create User</button>
-      {users.map((user) => {
-        return(
-          <div>
-            {" "}
-            <h1>Name: {user.name}</h1>
-            <h1>Age: {user.age}</h1>
-            <button 
-              onClick={() => {
-                upddateUser(user.id, user.age);
-              }}>
-              Increase Age
-            </button>
-            <button onClick={() => {deleteUser(user.id)}}>Delete User</button>
-          </div>
-        );
-      })}
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <input 
+          placeholder="Name..." 
+          onChange={(event) => {
+            this.setName(event.target.value);
+          }}
+        />
+        <input 
+          type ="number" 
+          placeholder="Age..."
+          onChange={(event) => {
+            this.setAge(event.target.value);
+          }}
+        />
+        <button onClick={this.createUser}>Create User</button>
+        {this.state.users.map((user) => {
+          return(
+            <div key={user.id}>
+              {" "}
+              <h1>Name: {user.name}</h1>
+              <h1>Age: {user.age}</h1>
+              <button 
+                onClick={() => {
+                  this.upddateUser(user.id, user.age);
+                }}>
+                Increase Age
+              </button>
+              <button onClick={() => {this.deleteUser(user.id)}}>Delete User</button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
